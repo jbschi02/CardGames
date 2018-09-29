29af57fb7ee760f92d1634129de30e6b0fbbd92f
jbschi02 <jbschi02@louisville.edu>
2018-09-29 00:33:26 -0400

    test commit on branch

--- CardGames/Team.cs
+++ CardGames/Team.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace CardGames
 {
     public class Team
     {
+        List<Player> players;
         public Team()
         {
+            players = new List<Player>();
         }
     }
 }
